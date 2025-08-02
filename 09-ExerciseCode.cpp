32a5ba73c813361af7ef6ed8a5295ccd1bdba487
RamonaEsL <ramonaczompa@yahoo.com>
2025-08-02 11:16:14 +0100

    09-Even

--- 09-ExerciseCode.cpp
+++ 09-ExerciseCode.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 using namespace std;
-
+                                                        I declared the function hello using  void to perform the action.the int main hello calls the function and executes the body
+                                                        
 void hello() {
     cout << "Hello" << endl;
 }
@@ -8,4 +9,5 @@ void hello() {
 int main() {
     hello();
     return 0;
+
 }
